662bef8ca07880e420c83c45d9bb905ac3727cca
samuelajayii <ajayisamuel023@gmail.com>
2025-08-29 17:13:57 +0100

    startup script test

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PORT="${PORT:-8080}"
-
-echo "Starting Next.js on port ${PORT}..."
-# start Next in production mode and ensure it listens on $PORT
-exec npx next start -p "${PORT}"
+echo "Starting Next.js on port $PORT..."
+exec npx next start -p "$PORT"
